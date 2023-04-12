f85c1fc3f271460ef8ad6ac7e33ce1d1dc8aee27
Tim977 <gentim977@gmail.com>
2023-04-11 22:59:50 -0400

    Authorisation - Created authorisation pages (sign in and sign up) - No db connection - No password check

--- main.py
+++ main.py
@@ -1,1 +1,58 @@
+from flask import Flask, render_template, redirect, request, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Login', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enter')
+
+class RegisterForm(FlaskForm):
+    username = StringField('Login', validators=[DataRequired()])
+    password_1 = PasswordField('Create Password', validators=[DataRequired()])
+    password_2 = PasswordField('Repeat the Password', validators=[DataRequired()])
+    submit = SubmitField('Enter')
+
+
+@app.route('/')
+def start():
+    return redirect('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    global username
+    global password
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = request.form['username']
+        password = request.form['password']
+        print(username, password)
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    global username
+    global password
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username = request.form['username']
+        password_1 = request.form['password_1']
+        password_2 = request.form['password_2']
+        print(username, password_1, password_2)
+        return redirect('/success')
+    return render_template('register.html', title='Авторизация', form=form)
+
+@app.route('/success')
+def success():
+    return render_template('success.html', username=username)
+
+
+if __name__ == '__main__':
+    app.run(port=5000, host='127.0.0.1')
